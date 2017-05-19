d9c63ce5094e29628110c72d068358cacb297895
Pranit Bauva <pranit.bauva@gmail.com>
2017-05-20 04:05:49 +0530

    add code for Ex. 6 on variables
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

---
+++ 05_variables.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+my_name = 'Pranit Bauva'
+my_age = 20
+my_height = 190
+my_weight = 81
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "His age is #{my_age}."
