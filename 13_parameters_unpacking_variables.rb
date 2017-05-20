f8bc3d031a56f44b7e9741549b4f6cb6516d8d6b
Pranit Bauva <pranit.bauva@gmail.com>
2017-05-20 17:58:27 +0530

    add "Parameters unpacking variables"
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

---
+++ 13_parameters_unpacking_variables.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# This unpacks `ARGV` into three variables.
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
