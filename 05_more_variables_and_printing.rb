e63c4dd55c7e6b3a7987583b5cbd1cb0eadf7320
Pranit Bauva <pranit.bauva@gmail.com>
2017-05-20 12:00:28 +0530

    improve the previous code
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

---
+++ 05_more_variables_and_printing.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+my_name = 'Pranit Bauva'
+my_age = 20
+my_height = 190
+my_weight = 81
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the tea."
+
+# This line is tricky, try to get it correct
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
