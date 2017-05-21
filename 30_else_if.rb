b9f97305a7743ec82182a17f6eb8847cf521d339
Pranit Bauva <pranit.bauva@gmail.com>
2017-05-21 18:18:08 +0530

    add "Else If"
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

---
+++ 30_else_if.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if people > trucks
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
